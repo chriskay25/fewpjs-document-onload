9ce5f278ee6c45ef498be906b1d480a9d8a68218
chriskay25 <chriskay25@gmail.com>
2020-06-08 12:19:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+// text = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+});
+
